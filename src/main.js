db4e399ecb09376900a664079b21c1d18f12df8e
Leopoldthecoder <Leopoldthecuber@gmail.com>
2016-10-30 17:52:06 +0800

    Init

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+import App from './App.vue'
+
+Vue.use(MintUI)
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
